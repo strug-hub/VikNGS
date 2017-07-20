0ea0e3f137250d5fcf20c345a45bda0b57521c80
ScottMastro <scott-mastro@hotmail.com>
2017-07-20 01:18:19 -0400

    included functionality for non-cov rare test
    migrated test object to new .h file

--- RVS/StatisticsHelper.cpp
+++ RVS/StatisticsHelper.cpp
@@ -1,107 +1,6 @@
 #include "stdafx.h"
 #include "RVS.h"
-#include <vector>
 #include <random>
-
-VectorXd extractRows(VectorXd &v, VectorXd &where, double equals) {
-	VectorXd subset(v.rows());
-	int c = 0;
-
-	for (int i = 0; i < where.rows(); i++) {
-		if (where[i] == equals) {
-			subset[c] = v[i];
-			c++;
-		}
-	}
-
-	return subset.block(0, 0, c, 1);
-}
-
-MatrixXd extractRows(MatrixXd &m, VectorXd &where, double equals) {
-	MatrixXd subset(m.rows(), m.cols());
-	int c = 0;
-	int i, j;
-
-	for (i = 0; i < where.rows(); i++) {
-		if (where[i] == equals) {
-			for (j = 0; j < m.cols(); j++)
-				subset(c, j) = m(i, j);
-			c++;
-		}
-	}
-
-	return subset.block(0, 0, c, m.cols());
-}
-
-VectorXd whereNAN(VectorXd &X, VectorXd &Y, MatrixXd &Z) {
-	int nobs = Y.rows();
-	int ncov = Z.cols();
-
-	VectorXd toRemove(nobs);
-	
-	for (int i = 0; i < nobs; i++) {
-		toRemove[i] = 0;
-
-		if (isnan(X[i]) || isnan(Y[i]))
-			toRemove[i] = 1;
-		else {
-			for (int j = 0; j < ncov; j++)
-				if (isnan(Z(i, j)))
-					toRemove[i] = 1;
-		}
-	}
-
-	return toRemove;
-}
-
-VectorXd whereNAN(VectorXd &Y, MatrixXd &Z) {
-	int nobs = Y.rows();
-	int ncov = Z.cols();
-
-	VectorXd toRemove(nobs);
-
-	for (int i = 0; i < nobs; i++) {
-		toRemove[i] = 0;
-
-		if (isnan(Y[i]))
-			toRemove[i] = 1;
-		else {
-			for (int j = 0; j < ncov; j++)
-				if (isnan(Z(i, j)))
-					toRemove[i] = 1;
-		}
-	}
-
-	return toRemove;
-}
-
-VectorXd whereNAN(VectorXd &X, VectorXd &Y) {
-	int nobs = X.rows();
-	int ncov = Y.cols();
-	VectorXd toRemove(nobs);
-
-	for (int i = 0; i < nobs; i++) {
-		toRemove[i] = 0;
-		if (isnan(X[i]) || isnan(Y[i]))
-			toRemove[i] = 1;
-	}
-	return toRemove;
-}
-
-VectorXd whereNAN(VectorXd &X) {
-	VectorXd toRemove(X.rows());
-	for (int i = 0; i < X.rows(); i++) {
-		toRemove[i] = 0;
-
-		if (isnan(X[i]))
-			toRemove[i] = 1;
-		else 
-			toRemove[i] = 0;
-	}
-
-	return toRemove;
-}
-
 
 inline VectorXd CovariateRegression(VectorXd &Y, MatrixXd &Z) {
 	return Z.householderQr().solve(Y);
@@ -132,33 +31,6 @@ std::vector<VectorXd> fitModel(VectorXd &beta, std::vector<VectorXd> &y, std::ve
 		ycenter.push_back(y[i] - meanValue);
 	}
 	return ycenter;
-}
-
-double average(std::vector<VectorXd> v) {
-	double sum = 0;
-	double n = 0;
-	for (int i = 0; i < v.size(); i++) {
-		n += v[i].size();
-		sum += v[i].sum();
-	}
-	return sum / n;
-}
-
-double variance(VectorXd &v) {
-
-	double mean = v.mean();
-	double var = (v.array() - mean).pow(2).sum();
-
-	return var/(v.rows()-1);
-}
-
-
-double center(VectorXd &v) {
-
-	double mean = v.mean();
-	double var = (v.array() - mean).pow(2).sum();
-
-	return var / (v.rows() - 1);
 }
 
 /*
@@ -315,15 +187,13 @@ double pnorm(double x)
 
 std::vector<double> randomSample(std::vector<double> &vec, int nsample) {
 	std::vector<double> rvec;
-	std::uniform_int_distribution<> sample(0, vec.size()-1);
+	std::uniform_int_distribution<> sample(0, vec.size() - 1);
 
 	for (size_t i = 0; i < nsample; i++)
 		rvec.push_back(vec[sample(generate)]);
-	
+
 	return rvec;
 }
-
-
 
 /*
 Finds p-value for test statistic using a chi-squared distribution with one degree of freedom
@@ -359,29 +229,4 @@ double chiSquareOneDOF(double statistic) {
 	p /= tgamma(0.5);
 
 	return 1 - p;
-}
-
-MatrixXd nanToZero(MatrixXd &M) {
-	std::cout << "\n";	std::cout << "\n";
-	std::cout << "\n";
-	std::cout << "\n";
-	std::cout << "\n";
-
-	std::cout << M;
-
-	for (size_t i = 0; i < M.cols(); i++) 
-		for (size_t j = 0; j < M.rows(); j++) 
-			if (isnan(M(j, i)))
-				M(j, i) = 0;
-
-	return  M;
-}
-
-VectorXd nanToZero(VectorXd &V) {
-
-	for (size_t i = 0; i < V.rows(); i++)
-			if (isnan(V[i]))
-				V[i] = 0;
-
-	return V;
 }
